45b970edec536046b750fa796dffae0def3f01ba
smprofixservices <smprofixservices@gmail.com>
2026-01-01 15:35:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected ✔'))
   .catch(err => console.error('MongoDB Error:', err));
 
-  .then(() => console.log('MongoDB Connected ✔'))
-  .catch(err => console.error('MongoDB Error:', err));
-
 /* ---------------- SENDGRID TRANSPORTER ---------------- */
 const transporter = nodemailer.createTransport({
   host: 'smtp.sendgrid.net',
